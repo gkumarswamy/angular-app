89ab2528b7addcfb30bf601f00029940a314adf2
Kumar <kumaraswamyg@sssbi.com>
2021-04-09 07:26:03 +0530

    added toastr and json server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { SignUpComponent } from './sign-up/sign-up.component';
   imports: [
     BrowserModule,
     FormsModule,
+    BrowserAnimationsModule,
     HttpClientModule,
+    ToastrModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
@@ -8,9 +9,17 @@ import { HttpClient } from '@angular/common/http';
 export class SignUpComponent implements OnInit {
   formObj:any ={name:"", username:"", password:""};
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+  }
+
+  showSuccess(data) {
+    this.toastr.success(data, 'Toastr fun!');
+  }
+  
+  showError(data) {
+    this.toastr.error(data, 'Toastr fun!');
   }
 
   onInsertEmployee(){
@@ -22,8 +31,16 @@ export class SignUpComponent implements OnInit {
   }
   
   onClickButton() {
-    this.onInsertEmployee();
-    console.log(this.formObj.username, this.formObj.password);
+    if(!this.formObj.name){
+      this.showError("Name is required");
+    } else if(!this.formObj.username){
+      this.showError("Username is required");
+    } else if(!this.formObj.password){
+      this.showError("Password is required");
+    } else {
+      this.onInsertEmployee();
+      console.log(this.formObj.username, this.formObj.password);
+    }
   }
 
 }
