c97cfbc12907e0808b8d1402feaf156dbb7fb85d
Kumar <kumaraswamyg@sssbi.com>
2021-04-12 07:25:03 +0530

    add new module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { MyappModule } from './myapp/myapp.module'
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
 import { ToastrModule } from 'ngx-toastr';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    SignUpComponent,
-    PageNotFoundComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -23,7 +18,8 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     BrowserAnimationsModule,
     HttpClientModule,
     ToastrModule.forRoot(),
-    AppRoutingModule
+    MyappModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
