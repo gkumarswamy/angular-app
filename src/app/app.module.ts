85b994c088988ad7af93427dde245da948b293b2
Kumar <kumaraswamyg@sssbi.com>
2021-04-13 07:00:09 +0530

    theme set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
 import { ToastrModule } from 'ngx-toastr';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    SignUpComponent,
-    PageNotFoundComponent
-  ],
+    AppComponent
+   ],
   imports: [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    ToastrModule.forRoot(),
-    AppRoutingModule
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
