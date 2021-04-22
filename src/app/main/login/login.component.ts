e96e5948eb2bf6e91635f162d3a5419fd335c2c1
Kumar <kumaraswamyg@sssbi.com>
2021-04-22 06:51:35 +0530

    added header UI

--- src/app/main/login/login.component.ts
+++ src/app/main/login/login.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  title:string='Login';
   constructor() { }
 
   ngOnInit(): void {
+    console.log('Lgom-oninit');
+  }
+
+  onSubmit(){
+    console.log('login page..');
   }
 
 }
